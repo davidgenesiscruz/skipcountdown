37151da8c5dd877bd97a923306f3f03248c89a36
David Genesis Cruz (timetosaygoodbye) <david.genesis.cruz@bizreach.co.jp>
2020-06-21 13:21:10 +0900

    added fixes for prime video update

--- contentscript/amazon.js
+++ contentscript/amazon.js
@@ -1,39 +1,35 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-const nextUpObserver = new MutationObserver((mutations) => {
-    const firstNewNode = mutations[0].addedNodes[0];
-    if (!firstNewNode) {
-        return;
-    }
-
-    const countdown = firstNewNode.querySelector('.countdown');
-    if (countdown) {
-        countdown.click();
-        return;
-    }
-});
-
-const apageObserver = new MutationObserver((mutations, observer) => {
+const webPlayerObserver = new MutationObserver((mutations, observer) => {
     mutations.forEach(mutation => {
         const newNodes = mutation.addedNodes;
         if (newNodes.length == 0) {
             return;
         }
 
-        Array.from(newNodes)
+        const nextUp = Array.from(newNodes)
             .filter(node => node instanceof Element)
             .forEach(node => {
-                const nextUp = node.querySelector('.nextUp');
-                if (nextUp) {
-                    nextUpObserver.observe(nextUp, { childList: true });
-                    observer.disconnect();
+                const allDivs = Array.from(node.querySelectorAll('div'));
+                const nextUp =  allDivs.find(node => node.textContent == 'Next Up');
+                const skip = allDivs.find(node => node.textContent == 'Skip');
+                if (!nextUp && !skip) {
+                    return;
                 }
+
+                if (nextUp) {
+                    nextUp.parentElement.nextElementSibling.click();
+                }
+                if (skip) {
+                    skip.click();
+                }
+                observer.disconnect();
             });
     });
 });
 
-const apage = document.querySelector('#a-page');
-if (apage) {
-    apageObserver.observe(apage, { childList: true, subtree: true });
+const webPlayer = document.querySelector('#dv-web-player');
+if (webPlayer) {
+    webPlayerObserver.observe(webPlayer, { childList: true, subtree: true });
 }
 
